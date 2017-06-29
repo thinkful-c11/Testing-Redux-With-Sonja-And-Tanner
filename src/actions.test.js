a5a301d19e2fa0ab4ce5e96f3b0d981c7140b720
tannaurus <contacttannergill@gmail.com>
2017-06-29 11:11:36 -0700

    All tests for actions completed.

---
+++ src/actions.test.js
@@ -0,0 +1,33 @@
+import {
+  newGame,
+  makeGuess,
+  toggleInfoModal,
+  NEW_GAME,
+  MAKE_GUESS,
+  TOGGLE_INFO_MODAL
+} from "./actions";
+
+describe("newGame", () => {
+  it("should return the action and should contain a number", () => {
+    const action = newGame();
+    expect(action.type).toEqual(NEW_GAME);
+    expect(typeof action.correctAnswer).toBe("number");
+  });
+});
+
+describe("makeGuess", () => {
+  it("should have the action and should contain a guess", () => {
+    const guess = 34;
+    const action = makeGuess(guess);
+    expect(action.type).toEqual(MAKE_GUESS);
+    expect(action.guess).toEqual(guess);
+  });
+});
+
+describe("toggleInfoModal", () => {
+  it("should have the action", () => {
+    const action = toggleInfoModal();
+    expect(action.type).toEqual(TOGGLE_INFO_MODAL);
+  });
+});
+
---
+++ src/reducer.test.js
@@ -0,0 +1,8 @@
+import Reducer from './reducer';
+import {makeGuess, MAKE_GUESS, toggleInfoModal, TOGGLE_INFO_MODAL, newGame, NEW_GAME} from './actions';
+
+describe("Reducer", () => {
+  it("newGame should reset state.", ()=>{
+
+  })
+})
